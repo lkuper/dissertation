fa5c9084be0431c8878be5fe13bc1ceb3da34637
Lindsey Kuper <lindsey@composition.al>
2014-06-28 21:24:03 -0400

    Edits to 2.0 and 2.1.

--- diss/chapter2/code/bfs_pure.hs
+++ diss/chapter2/code/bfs_pure.hs
@@ -1,12 +1,10 @@
 nbrs :: Graph -> NodeLabel -> Set NodeLabel
--- 'nbrs g n' is the neighbor nodes of `n' in `g'
+-- `nbrs g n' is the neighbor nodes of node `n' in graph `g'.
 
--- Traverse each level of the graph in parallel,
--- maintaining at each recursive step a set of
--- nodes that have been seen and a set of nodes
--- left to process.
-bf_traverse :: Graph -> Set NodeLabel ->
-               Set NodeLabel -> Set NodeLabel
+-- Traverse each level of the graph in parallel, maintaining at each
+-- recursive step a set of nodes that have been seen and a set of
+-- nodes left to process.
+bf_traverse :: Graph -> Set NodeLabel -> Set NodeLabel -> Set NodeLabel
 bf_traverse g seen nu = 
   if nu == S.empty 
   then seen
@@ -15,7 +13,6 @@ bf_traverse g seen nu =
            nu'    = difference allNbr seen'
        in bf_traverse g seen' nu'
 
--- Next we traverse the connected component, 
--- starting with the vertex `profile0':
+-- Next we traverse the connected component, starting with the vertex `profile0':
 ccmp = bf_traverse profiles S.empty     profile0Sing
 result = parMap analyze ccmp
