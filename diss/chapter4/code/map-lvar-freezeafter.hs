e6713659475bccafa937dbb3ba310ab089bfd593
Lindsey Kuper <lindsey@composition.al>
2014-08-10 19:06:34 -0700

    More work on 4.2, plus some non-local effects.

---
+++ diss/chapter4/code/map-lvar-freezeafter.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Control.LVish
+import Control.LVish.DeepFrz
+import Data.LVar.PureMap
+
+data Item = Book | Shoes
+  deriving (Show, Ord, Eq)
+
+p :: (HasPut e, HasGet e) => Par e s (IMap Item s Int)
+p = do
+  cart <- newEmptyMap
+  fork (insert Book 2 cart)
+  fork (insert Shoes 1 cart)
+  return cart
+
+main = print (runParThenFreeze p)
---
+++ diss/chapter4/code/map-lvar-getkey-lib.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Control.LVish
+import Data.LVar.PureMap
+
+data Item = Book | Shoes
+  deriving (Show, Ord, Eq)
+
+p :: (HasPut e, HasGet e) => Par e s Int
+p = do
+  cart <- newEmptyMap
+  fork (insert Book 2 cart)
+  fork (insert Shoes 1 cart)
+  getKey Book cart
+
+main = print (runPar p)
