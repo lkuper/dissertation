472837a825307450d57f3ab9826e1f24d6aa2415
Ryan Newton <rrnewton@gmail.com>
2014-12-22 14:12:57 -0500

    Minor: let code spread out more in terms of width

--- dissertation/chapter4/code/repeated-4-lvar.hs
+++ dissertation/chapter4/code/repeated-4-lvar.hs
@@ -5,10 +5,9 @@ import Control.LVish  -- Generic scheduler; works with all LVars.
 import Data.LVar.IVar -- The particular LVar we need for this program.
 
 p :: (HasPut e, HasGet e) => Par e s Int
-p = do
-  num <- new
-  fork (put num 4)
-  fork (put num 4)
-  get num
+p = do num <- new
+       fork (put num 4)
+       fork (put num 4)
+       get num
 
 main = print (runPar p)
