846099eb328e3b057bed51dd07ef0fbf38b520a9
Lindsey Kuper <lindsey@composition.al>
2015-01-18 18:00:33 -0800

    Chapter/Section 2 wording edits.

--- dissertation/chapter2/code/bfs_pure.hs
+++ dissertation/chapter2/code/bfs_pure.hs
@@ -1,7 +1,7 @@
 nbrs :: Graph -> NodeLabel -> Set NodeLabel
 -- `nbrs g n' is the neighbor nodes of node `n' in graph `g'.
 
--- Traverse each level of the graph in parallel, maintaining at each
+-- Traverse each level of the graph `g' in parallel, maintaining at each
 -- recursive step a set of nodes that have been seen and a set of
 -- nodes left to process.
 bf_traverse :: Graph -> Set NodeLabel -> Set NodeLabel -> Set NodeLabel
@@ -13,7 +13,7 @@ bf_traverse g seen nu =
            nu'    = difference allNbr seen'
        in bf_traverse g seen' nu'
 
--- Next we traverse the connected component, starting with the vertex
--- `profile0':
-ccmp = bf_traverse profiles empty (singleton profile0)
-result = parMap analyze ccmp
+-- Find the connected component containing the vertex `profile0', and
+-- map `analyze` over all nodes in it:
+connected_component = bf_traverse profiles empty (singleton profile0)
+result = parMap analyze connected_component
