91f66017350e9b73a39d74e0cfa97a152c225537
Lindsey Kuper <lindsey@composition.al>
2015-03-04 23:40:28 -0800

    Edits to 4.2.3 and 4.2.4.

--- dissertation/chapter4/code/map-lvar-getkey-lib.hs
+++ dissertation/chapter4/code/map-lvar-getkey-lib.hs
@@ -2,10 +2,9 @@ import Control.LVish
 import Data.LVar.PureMap
 
 p :: (HasPut e, HasGet e) => Par e s Int
-p = do
-  cart <- newEmptyMap
-  fork (insert Book 2 cart)
-  fork (insert Shoes 1 cart)
-  getKey Book cart
+p = do cart <- newEmptyMap
+       fork (insert Book 2 cart)
+       fork (insert Shoes 1 cart)
+       getKey Book cart
 
 main = print (runPar p)
--- dissertation/chapter4/code/map-lvar-quasi.hs
+++ dissertation/chapter4/code/map-lvar-quasi.hs
@@ -4,12 +4,10 @@ import Data.LVar.PureMap
 import qualified Data.Map as M
 
 p :: (HasPut e, HasFreeze e) => Par e s (M.Map Item Int)
-p = do
-  cart <- newEmptyMap
-  fork (insert Book 2 cart)
-  fork (insert Shoes 1 cart)
-  freezeMap cart
+p = do cart <- newEmptyMap
+       fork (insert Book 2 cart)
+       fork (insert Shoes 1 cart)
+       freezeMap cart
 
-main = do
-  v <- runParQuasiDet p
-  print (M.toList v)
+main = do v <- runParQuasiDet p
+          print (M.toList v)
