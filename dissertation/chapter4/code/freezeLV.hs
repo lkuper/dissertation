31a80d14b87b539085a79719453366f381bd8936
Lindsey Kuper <lindsey@composition.al>
2015-07-12 22:23:33 -0700

    Get rid of some no-longer-wanted code from Chapter 4.

--- dissertation/chapter4/code/freezeLV.hs
+++
@@ -1,9 +0,0 @@
-freezeLV :: LVar a d -> Par QuasiDet ()
-freezeLV (LVar {status}) = mkPar $ \k q -> do
-  Sched.awaitClear q
-  oldStat <- atomicModifyIORef status $ \s->(Frozen, s)    
-  case oldStat of
-    Frozen -> return ()
-    Active listeners -> B.foreach listeners $ 
-      \(Listener _ onFrz) tok -> onFrz tok q
-  k () q
