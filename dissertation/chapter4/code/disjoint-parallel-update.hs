844108c4ffd8d9a8675e3489808940e3bb559e00
Lindsey Kuper <lindsey@composition.al>
2015-07-13 01:43:13 -0700

    Long-awaited revisions to section 4.3.

--- dissertation/chapter4/code/disjoint-parallel-update.hs
+++ dissertation/chapter4/code/disjoint-parallel-update.hs
@@ -1,23 +1,26 @@
+{-# LANGUAGE TypeFamilies #-}
+
+import Prelude hiding (read)
 import Control.LVish
-import qualified Control.Par.ST as PST
-import qualified Control.Par.ST.Vec as V
+import Control.Par.ST (liftST)
+import Control.Par.ST.Vec (ParVecT, set, reify, forkSTSplit, write, read, runParVecT)
 import Data.Vector (freeze, toList)
 
-p :: (HasGet e, HasPut e) => V.ParVecT s1 String Par e s [String]
+p :: (HasGet e, HasPut e) => ParVecT s1 String Par e s [String]
 p = do
   -- Fill all six slots in the vector with "foo".
-  V.set "foo"
+  set "foo"
   -- Get a pointer to the state.
-  ptr <- V.reify
+  ptr <- reify
 
   -- Fork two computations, each of which has access to half the
   -- vector.  Within the two forked child computations, `ptr` is
   -- inaccessible.
-  V.forkSTSplit 3 -- Split at index 3 in the vector.
-                (V.write 0 "bar")
-                (V.write 0 "baz")
+  forkSTSplit 3 -- Split at index 3 in the vector.
+              (write 0 "bar")
+              (write 0 "baz")
 
-  frozen <- PST.liftST (freeze ptr)
+  frozen <- liftST (freeze ptr)
   return (toList frozen)
 
-main = print (runPar (V.runParVecT 6 p))
+main = print (runPar (runParVecT 6 p))
