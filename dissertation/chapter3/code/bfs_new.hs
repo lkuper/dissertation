6165f9edd14a7bd169c3b12340989214eb021bd7
Lindsey Kuper <lindsey@composition.al>
2015-07-15 23:50:46 -0700

    Update Chapter 3 graph traversal example to match the later one.
    Probably needs to be read over again.

--- dissertation/chapter3/code/bfs_new.hs
+++ dissertation/chapter3/code/bfs_new.hs
@@ -1,6 +1,9 @@
-traverse :: Graph -> NodeLabel -> Par (Set NodeLabel)
 traverse g startNode = do
   seen <- newEmptySet
+  h <- newHandler seen
+       (\node -> do
+           mapM (\v -> insert v seen) (neighbors g node)
+           return ())
   insert startNode seen -- Kick things off
-  let handle node = mapM (\v -> insert v seen) (neighbors g node)
-  freezeSetAfter seen handle
+  quiesce h
+  freeze seen
